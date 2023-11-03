2fd88c1d8cb4b271b2e93eaff4b51ea8cff389f7
zhangatle <zhangatle@gmail.com>
2023-11-03 14:22:55 +0800

    fix

--- ql/ql_jindian_sign.js
+++ ql/ql_jindian_sign.js
@@ -1,10 +1,5 @@
 /*
-领京豆升级任务
-活动入口：京东APP首页-领京豆任务
-cron "25 2,11 * * *" script-path=jd_bean_home.js, tag=领京豆升级任务
-
-请使用本地IP环境 请使用本地IP环境 请使用本地IP环境
-
+cron "25 2,10 * * *" script-path=jd_bean_home.js, tag=领京豆升级任务
  */
 
 const axios = require('axios');
